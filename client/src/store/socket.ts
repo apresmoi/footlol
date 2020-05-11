9bf7e8277d5f647fd6dcbc0059ed78e069992be3
Juan Cruz Fortunatti <juancfortunatti@gmail.com>
2020-05-11 02:23:01 -0300

    Build

--- client/src/store/socket.ts
+++ client/src/store/socket.ts
@@ -21,7 +21,7 @@ class RoomSocket {
     _onMessageSubscribers: MessageSubscribers = {}
 
     constructor(roomId, name) {
-        this._chatSocket = io(window.location.host.replace('8000', '8081') + `${roomId}?name=${name}`, {
+        this._chatSocket = io(window.location.host.replace(':8000', '') + ':8081' + `${roomId}?name=${name}`, {
             path: `/ws`,
             autoConnect: false
         });
