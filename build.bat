ce9d6ce980e2a72e935bc39ca9643e66f4969ae7
Juan Cruz Fortunatti <juancfortunatti@gmail.com>
2020-05-19 01:18:57 -0300

    version

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-cd client & npm run build & cd .. & del .\nginx\public\* /s /q & xcopy .\client\build .\nginx\public\ /s /e /y
+cd client & npm run build & cd .. & del .\nginx\public\* /s /q & xcopy .\client\build .\nginx\public\ /s /e /y & python build.py
